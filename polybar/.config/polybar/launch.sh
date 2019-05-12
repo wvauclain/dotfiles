6d7217110671a8f163ec3cb9698e6c2f35d2cc57
Will Vauclain <willvauclain.dev@gmail.com>
2019-05-11 21:27:35 -0400

    Streamlined per-machine configuration, consolidated in bspwmrc

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,15 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch the correct bars for the given computer
-case $(hostname) in
-    goldenarches)
-        polybar goldenarches &
-        ;;
-    shadowboxpc)
-        polybar shadowboxpc-main &
-        polybar shadowboxpc-side &
-        ;;
-esac
+# Launch all the bars passed as arguments
+for BAR in $@
+do
+    polybar $BAR &
+done
 
 echo "Bars launched..."
