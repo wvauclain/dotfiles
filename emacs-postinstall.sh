4697e4ec293d796a2df1f85c5dff20abc8196c8c
Will Vauclain <willvauclain.dev@gmail.com>
2020-05-14 21:07:06 -0400

    Migrated to doom-emacs

--- emacs-postinstall.sh
+++ emacs-postinstall.sh
@@ -1,24 +1,6 @@
-[ ! "$1" = "xbps" ] && exit
-
-if ! PATH="~/.wspm/bin:PATH" which ag || $UPDATE; then
-    mkdir -p tmp/ag
-    cd tmp/ag
-    cat > the_silver_searcher.toml <<EOF
-[metadata]
-package_name = "the_silver_searcher"
-version = "2.2.0"
-description = "A code-searching tool similar to ack, but faster."
-
-[files]
-binaries = ["the_silver_searcher/ag"]
-
-[commands]
-prepare = """git clone https://github.com/ggreer/the_silver_searcher.git
-cd the_silver_searcher
-git checkout 2.2.0"""
-build = """cd the_silver_searcher
-./build.sh"""
-EOF
-
-    ~/.wspm/bin/wspm --noconfirm install the_silver_searcher.toml
+if ! [ -d "$HOME/.emacs.d" ]; then
+    git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+    ~/.emacs.d/bin/doom install
+else
+    echo "Skipping installing doom to ~/.emacs.d..." >&2
 fi
