33cdc49324d7454d61b0600e41f4661553961272
Will Vauclain <willvauclain.dev@gmail.com>
2020-05-24 15:30:33 -0400

    Added novastorm to aconfmgr config

--- aconfmgr/.config/aconfmgr/01-packages.sh
+++ aconfmgr/.config/aconfmgr/01-packages.sh
@@ -3,7 +3,6 @@ AddPackage base # Minimal package set to define a basic Arch Linux installation
 AddPackage efibootmgr # Linux user-space application to modify the EFI Boot Manager
 AddPackage fwupd # Simple daemon to allow session software to update firmware
 AddPackage grub # GNU GRand Unified Bootloader (2)
-AddPackage intel-ucode # Microcode update files for Intel CPUs
 AddPackage linux # The Linux kernel and modules
 AddPackage linux-firmware # Firmware files for Linux
 AddPackage man-db # A utility for reading man pages
@@ -12,8 +11,27 @@ AddPackage networkmanager # Network connection manager and user applications
 AddPackage ntp # Network Time Protocol reference implementation
 AddPackage openssh # Premier connectivity tool for remote login with the SSH protocol
 AddPackage posix-user-portability # metapackage providing the POSIX shell and utilities
-AddPackage tlp # Linux Advanced Power Management
 AddPackageGroup base-devel
+
+# System-specific drivers
+if [ "$HOSTNAME" = howlback ]; then
+    AddPackage acpilight # a backward-compatible xbacklight replacement based on ACPI
+    AddPackage intel-ucode # Microcode update files for Intel CPUs
+    AddPackage mesa # An open-source implementation of the OpenGL specification
+    AddPackage vulkan-intel # Intel's Vulkan mesa driver
+    AddPackage xf86-video-vesa # X.org vesa video driver
+elif [ "$HOSTNAME" = novastorm ]; then
+    AddPackage amd-ucode # Microcode update files for AMD CPUs
+    AddPackage lib32-nvidia-utils # NVIDIA drivers utilities (32-bit)
+    AddPackage nvidia # NVIDIA drivers for linux
+    AddPackage nvidia-settings # Tool for configuring the NVIDIA graphics driver
+    AddPackage nvidia-utils # NVIDIA drivers utilities
+
+    CopyFile /etc/X11/xorg.conf.d/20-nvidia.conf
+else
+    echo "error: add microcode and graphics drivers for system $HOSTNAME" >&2
+    exit 1
+fi
 
 # Development
 AddPackage clang # C language family frontend for LLVM
@@ -38,24 +56,23 @@ AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and 
 AddPackage vim # Vi Improved, a highly configurable, improved version of the vi text editor
 
 # PER
-AddPackage arm-none-eabi-gdb # The GNU Debugger for the ARM EABI (bare-metal) target
 AddPackage bazel # Correct, reproducible, and fast builds for everyone
 AddPackage moserial # Clean, friendly GTK+-based serial terminal for the GNOME desktop
 AddPackage python-protobuf # Python 3 bindings for Google Protocol Buffers
 AddPackage python-pyserial # Multiplatform Serial Port Module for Python
-AddPackage --foreign gcc-arm-none-eabi-bin # GNU Tools ARM Embedded Processors (binary distribution, includes newlib, does NOT include GDB)
 
 # QMK
+AddPackage arm-none-eabi-binutils # A set of programs to assemble and manipulate binary and object files for the ARM EABI (bare-metal) target
+AddPackage arm-none-eabi-gcc # The GNU Compiler Collection - cross compiler for ARM EABI (bare-metal) target
+AddPackage arm-none-eabi-newlib # A C standard library implementation intended for use on embedded systems (ARM bare metal)
 AddPackage avr-binutils # A set of programs to assemble and manipulate binary and object files for the AVR architecture
 AddPackage avr-gcc # The GNU AVR Compiler Collection
 AddPackage avr-libc # The C runtime library for the AVR family of microcontrollers
+AddPackage avrdude # Download/upload/manipulate the ROM and EEPROM contents of AVR microcontrollers
 AddPackage dfu-programmer # Programmer for Atmel chips with a USB bootloader
 AddPackage dfu-util # Tool intended to download and upload firmware using DFU protocol to devices connected over USB
 
 # X.org
-AddPackage mesa # An open-source implementation of the OpenGL specification
-AddPackage vulkan-intel # Intel's Vulkan mesa driver
-AddPackage xf86-video-vesa # X.org vesa video driver
 AddPackage xorg-server # Xorg X server
 AddPackage xorg-setxkbmap # Set the keyboard using the X Keyboard Extension
 AddPackage xorg-xev # Print contents of X events
@@ -67,7 +84,6 @@ AddPackage xorg-xrdb # X server resource database utility
 AddPackage xorg-xset # User preference utility for X
 
 # WM
-AddPackage acpilight # a backward-compatible xbacklight replacement based on ACPI
 AddPackage bspwm # Tiling window manager based on binary space partitioning
 AddPackage dunst # Customizable and lightweight notification-daemon
 AddPackage feh # Fast and light imlib2-based image viewer
@@ -97,6 +113,7 @@ AddPackage syncthing # Open Source Continuous Replication / Cluster Synchronizat
 AddPackage vlc # Multi-platform MPEG, VCD/DVD, and DivX player
 AddPackage zathura # Minimalistic document viewer
 AddPackage zathura-pdf-mupdf # PDF support for Zathura (MuPDF backend) (Supports PDF, ePub, and OpenXPS)
+AddPackage --foreign qdirstat # Qt-based directory statistics (KDirStat/K4DirStat without any KDE - from the original KDirStat author)
 AddPackage --foreign slack-desktop # Slack Desktop (Beta) for Linux
 
 # 3D Printing
@@ -140,13 +157,30 @@ AddPackage noto-fonts-emoji # Google Noto emoji fonts
 AddPackage ttf-dejavu # Font family based on the Bitstream Vera Fonts with a wider range of characters
 AddPackage --foreign nerd-fonts-iosevka # Patched Iosevka font from the nerd-fonts library
 
-# Install batt
+# Optional dependencies for added features
+AddPackage highlight # Syntax highlighting for ranger and trizen
+
+# System-specific apps and utilities
 if [ "$HOSTNAME" = howlback ]; then
+    # Install and enable tlp
+    AddPackage tlp # Linux Advanced Power Management
+    CopyFile /etc/tlp.conf
+    CreateLink /etc/systemd/system/multi-user.target.wants/tlp.service /usr/lib/systemd/system/tlp.service
+    CreateLink /etc/systemd/system/sleep.target.wants/tlp-sleep.service /usr/lib/systemd/system/tlp-sleep.service
+
+    # Install batt
     which batt > /dev/null 2>&1 || (
         cd $(mktemp -d batt.XXX)
         wget "https://raw.githubusercontent.com/wvauclain/batt/master/PKGBUILD"
         makepkg -si PKGBUILD
     )
-
     IgnorePackage --foreign batt
+
+elif [ "$HOSTNAME" = novastorm ]; then
+    AddPackage dosbox # Emulator with builtin DOS for running DOS Games
+    AddPackage handbrake # Multithreaded video transcoder
+    AddPackage libdvdcss # Portable abstraction library for DVD decryption
+    AddPackage sound-juicer # A lean and friendly audio CD extractor for GNOME
+    AddPackage steam # Valve's digital software delivery system
+    AddPackage --foreign makemkv # DVD and Blu-ray to MKV converter and network streamer
 fi
--- aconfmgr/.config/aconfmgr/20-config-files.sh
+++ aconfmgr/.config/aconfmgr/20-config-files.sh
@@ -1,7 +1,5 @@
-# TODO: fix "resume" parameter to work for multiple machines
 CopyFileTo "/etc/default/grub-$HOSTNAME" /etc/default/grub
 
 CopyFile /etc/pacman.conf
 CopyFile /etc/pacman.d/mirrorlist
 CopyFile /etc/sudoers
-CopyFile /etc/tlp.conf
