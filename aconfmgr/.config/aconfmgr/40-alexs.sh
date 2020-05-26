0eabf104c867f2c113ebebb3e63db8c432f4f097
Will Vauclain <willvauclain.dev@gmail.com>
2020-05-26 17:18:20 -0400

    Install dependencies for refine.io

---
+++ aconfmgr/.config/aconfmgr/40-alexs.sh
@@ -0,0 +1,13 @@
+AddPackage dash # POSIX compliant shell that aims to be as small as possible
+AddPackage docker # Pack, ship and run any application as a lightweight container
+AddPackage git-crypt # Transparent file encryption in Git
+AddPackage jq # Command-line JSON processor
+AddPackage python-black # Uncompromising Python code formatter
+AddPackage python-pre-commit # A framework for managing and maintaining multi-language pre-commit hooks
+AddPackage qtkeychain # Provides support for secure credentials storage
+
+IgnorePath '/etc/docker/*'
+IgnorePath '/opt/containerd/*'
+CreateLink /etc/systemd/system/multi-user.target.wants/docker.service /usr/lib/systemd/system/docker.service
+CopyFile /usr/bin/nomad 755
+CopyFile /usr/bin/terraform 755
