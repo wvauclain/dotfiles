22981cb99903323c249561708203d0293dbd85f6
Will Vauclain <willvauclain.dev@gmail.com>
2020-06-09 13:49:45 -0400

    Remove emacs config

--- emacs-postinstall.sh
+++
@@ -1,6 +0,0 @@
-if ! [ -d "$HOME/.emacs.d" ]; then
-    git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-    ~/.emacs.d/bin/doom install
-else
-    echo "Skipping installing doom to ~/.emacs.d..." >&2
-fi
